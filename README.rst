54bc6f1693291871b4e3334bb0ab8a12ebd160e9
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2020-10-10 13:55:16 -0600

    Add LICENSE, readme, setup

---
+++ README.rst
@@ -0,0 +1,8 @@
+======
+symopt
+======
+
+After years of using SymPy to optimize expressions, it is time to begin formalizing some of the optimizations I use it for into a library. 
+This library will be focused on development work only - i.e. it would never be a dependency of another package. 
+
+Development started 2020-10-10; the library is much too immature for use by others at this point, and may well be for years or forever. 
---
+++ setup.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+'''Chemical Engineering Design Library (ChEDL). Utilities for process modeling.
+Copyright (C) 2020, Caleb Bell <Caleb.Andrew.Bell@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.'''
+
+from setuptools import setup
+
+classifiers = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+    'Intended Audience :: Manufacturing',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Operating System :: MacOS',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: POSIX',
+    'Operating System :: POSIX :: BSD',
+    'Operating System :: POSIX :: Linux',
+    'Operating System :: Unix',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Programming Language :: Python :: Implementation :: PyPy',
+    'Topic :: Education',
+    'Topic :: Scientific/Engineering :: Mathematics',
+]
+
+description = 'Library for mathematical optimization for sympy'
+keywords = ('math optimization sympy')
+
+setup(
+    name='symopt',
+    packages=['symopt'],
+    license='MIT',
+    version='0.1.1',
+    download_url='https://github.com/CalebBell/sympopt/tarball/0.1.1',
+    description=description,
+    long_description=open('README.rst').read(),
+    install_requires=["sympy"],
+    author='Caleb Bell',
+    author_email='Caleb.Andrew.Bell@gmail.com',
+    platforms=['Windows', 'Linux', 'Mac OS', 'Unix'],
+    url='https://github.com/CalebBell/symopt',
+    keywords=keywords,
+    classifiers=classifiers,
+    package_data={
+        'fluids': [
+            'David_Wilson_powers/*',
+        ]
+    },
+)
