662d54a5c9487f14fc8cd924732f4e7929918875
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2020-10-11 14:18:34 -0600

    Add simplify_powers_as_fractions

--- symopt/optimize_terms.py
+++ symopt/optimize_terms.py
@@ -26,10 +26,12 @@ from __future__ import division
 from sympy import *
 from math import isclose
 from sympy.core import Add, Mul, Number
+from .addition_chain import minimum_addition_chain_multi_heuristic
 
 __all__ = ['replace_inv', 'replace_power_sqrts', 'horner_expr',
            'optimize_expression_for_var', 'optimize_expression',
-           'recursive_find_power', 'make_pow_sym', 'replace_intpowers']
+           'recursive_find_power', 'make_pow_sym', 'replace_intpowers',
+           'integer_chain_symbolic_path', 'simplify_powers_as_fractions']
 
 def replace_inv(expr, var, var_inv):
     '''Accepts and expression, and replaces a specified variable and replaces
@@ -187,6 +189,31 @@ def recursive_find_power(expr, var, powers=None, selector=lambda x: True):
             recursive_find_power(arg, var, powers, selector)
     return powers
 
+def simplify_powers_as_fractions(expr, var, max_denom=1000):
+    '''Takes an expression and replaces
+    
+    >>> x = symbols('x')
+    >>> expr = x**.15 + x**.2 + x**.33 + x**.35 + x**.8 + x**1.01 + x**1.6
+    >>> simplify_powers_as_fractions(expr, x)
+    x**(101/100) + x**(33/100) + x**(7/20) + x**(3/20) + x**(8/5) + x**(4/5) + x**(1/5)
+    >>> expr = x**.15
+    >>> simplify_powers_as_fractions(expr, x)
+    x**(3/20)
+    '''
+    def change_term(arg):
+        coeff, exponent = arg.as_coeff_exponent(var)
+        exponent_simplified = nsimplify(exponent)
+        if exponent_simplified.denominator() <= max_denom:
+            return expr.replace(var**exponent, var**exponent_simplified)
+        return expr
+    
+    if isinstance(expr, Add):
+        for i in range(len(expr.args)):
+            expr = change_term(expr.args[i])
+    elif isinstance(expr, Mul) or isinstance(expr, Pow):
+        expr = change_term(expr)
+    return expr
+
 def horner_expr(expr, var):
     '''Basic wrapper around sympy's horner which does not raise an exception if
     there is nothing to do.
@@ -212,44 +239,97 @@ def make_pow_sym(var, power):
     >>> x = symbols('x')
     >>> make_pow_sym(x, 100)
     x100
+    >>> make_pow_sym(x, 1)
+    x
     '''
+    if power == 1:
+        return var
     name = var.name + str(power)
     sym = symbols(name)
     return sym
 
+def integer_chain_symbolic_path(chain, var):
+    '''Returns a tuple of assignments, expressions which can be joined together
+    to calculate all of the necessary powers for an operation.
+    
+    >>> x = symbols('x')
+    >>> chain = [[2], [2, 3], [2, 3, 5, 10, 13], [2, 3, 5, 10, 20]]
+    >>> integer_chain_symbolic_path(chain, x)
+    ([x2, x3, x5, x10, x13, x20], [x*x, x*x2, x2*x3, x5*x5, x3*x10, x10*x10])
+    '''
+    assignments = []
+    expressions = []
+    for l in chain:
+        for i, v in enumerate(l):
+            to_add_asign = make_pow_sym(var, v)
+            if i == 0:
+                assert v == 2
+                to_add_expr = UnevaluatedExpr(var)*var
+            else:
+                prev = l[i-1]
+                delta = v-l[i-1]
+                to_add_expr = UnevaluatedExpr(make_pow_sym(var, prev))*make_pow_sym(var, delta)
+            
+            if to_add_asign not in assignments:
+                assignments.append(to_add_asign)
+                expressions.append(to_add_expr)
+    return assignments, expressions
+
+    
+    
 def replace_intpowers(expr, var):
     '''
     >>> x, y = symbols('x, y')
     >>> test = x**2*sin(x**3)*y+y*x**20
-    >>> replace_intpowers(test, x)
+    >>> replace_intpowers(test, x)[0]
     x2*y*sin(x3) + x20*y
-    >>> replace_intpowers(test, y)
+    >>> replace_intpowers(test, x)[1]
+    [x2, x3, x5, x10, x20]
+    >>> replace_intpowers(test, x)[2]    
+    [x*x, x*x2, x2*x3, x5*x5, x10*x10]
+    >>> replace_intpowers(test, y)[0]
     x**20*y + x**2*y*sin(x**3)
     '''
     powers = list(sorted(list(recursive_find_power(expr, var, selector=lambda x: int(x) == x))))
-#     chain = minimum_addition_chain_multi_heuristic(powers, small_chain_length=6)
+    powers_int = [int(i) for i in powers]
+    chain_length, chain = minimum_addition_chain_multi_heuristic(powers_int, small_chain_length=6)
+    assignments, expressions = integer_chain_symbolic_path(chain, var)
     replacement_vars = [make_pow_sym(var, p) for p in powers]
     for power, replacement in zip(powers[::-1], replacement_vars[::-1]):
         # iterate from highest to low
         expr = expr.subs(var**power, replacement)
-    return expr
+    return expr, assignments, expressions
 
-def optimize_expression_for_var(expr, var, var_inv, horner=True):
+def optimize_expression_for_var(expr, var, var_inv, horner=True, intpows=True):
     expr = replace_inv(expr, var, var_inv)
     if horner:
         expr = horner_expr(expr, var)
         expr = horner_expr(expr, var_inv)
     expr = replace_power_sqrts(expr, var)
-    return expr
+    assignments = []
+    expressions = []
+    if intpows:
+        expr, assign_tmp, expr_tmp = replace_intpowers(expr, var)
+        assignments += assign_tmp
+        expressions += expr_tmp
+        expr, assign_tmp, expr_tmp = replace_intpowers(expr, var_inv)
+        assignments += assign_tmp
+        expressions += expr_tmp
+    return expr, assignments, expressions
 
-def optimize_expression(expr, variables, inverse_variables, horner=True):
+def optimize_expression(expr, variables, inverse_variables, horner=True,
+                        intpows=True):
     '''
     >>> tau, delta, tau_inv, delta_inv = symbols('tau, delta, tau_inv, delta_inv')
     >>> expr = 17.2752665749999998*tau - 0.000195363419999999995*tau**1.5 + log(delta) + 2.49088803199999997*log(tau) + 0.791309508999999967*log(1 - exp(-25.36365*tau)) + 0.212236767999999992*log(1 - exp(-16.90741*tau)) - 0.197938903999999999*log(exp(87.31279*tau) + 0.666666666666667) - 13.8419280760000003 - 0.000158860715999999992/tau - 0.0000210274769000000003/tau**2 + 6.05719400000000021e-8/tau**3
-    >>> optimize_expression(expr, [tau,delta], [tau_inv, delta_inv])
+    >>> optimize_expression(expr, [tau,delta], [tau_inv, delta_inv])[0]
     17.275266575*tau - 0.00019536342*tau*sqrt(tau) + tau_inv*(tau_inv*(6.057194e-8*tau_inv - 2.10274769e-5) - 0.000158860716) + log(delta) + 2.490888032*log(tau) + 0.791309509*log(1 - exp(-25.36365*tau)) + 0.212236768*log(1 - exp(-16.90741*tau)) - 0.197938904*log(exp(87.31279*tau) + 0.666666666666667) - 13.841928076
     
     '''
+    assignments = []
+    expressions = []
     for var, var_inv in zip(variables, inverse_variables):
-        expr = optimize_expression_for_var(expr, var, var_inv, horner=horner)
-    return expr
+        expr, assign_tmp, expr_tmp = optimize_expression_for_var(expr, var, var_inv, horner=horner, intpows=intpows)
+        assignments += assign_tmp
+        expressions += expr_tmp
+    return expr, assignments, expressions
